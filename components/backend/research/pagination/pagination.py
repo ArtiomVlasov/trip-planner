0ee754ad7d9e54b491d0079ca25d5ea71b86bdad
Andrew Ivanov <106903166+F1ameX@users.noreply.github.com>
2025-07-04 23:30:37 +0700

    GEMINI->API pull request (#35)
    * minor changes + AI->API base added
    
    * simple nav added
    
    * minor changes

--- components/backend/research/pagination/pagination.py
+++ components/backend/research/pagination/pagination.py
@@ -15,24 +15,23 @@ headers = {
     "X-Places-Api-Version": "2025-06-17"
 }
 
-
 params = {
-    "ll": "40.18733549949229,44.51515940228367",
+    "ll": "54.93728052204032,83.10924773982785",
     "radius" : 1000,
     "limit" : 50, # Maximum possible with pagination. If set to 49, the next page will have only 1 result (pagination ends at 50).
     # "fields" : "price" - Available only in paid version of the API.
-    "min_price" : 2, # Prices range, where 1 - cheap and 4 - expensive. 
-    "max_price" : 3,
-    # "sort": "relevance, rating, distance, popularity", Params of sorting order, relevance set as default variable
+    # "min_price" : 2, # Prices range, where 1 - cheap and 4 - expensive. 
+    # "max_price" : 3,
+    "sort": "popularity", # Params of sorting order, relevance set as default variable
     # "open_now" : True, # Return only currently open POIs by working hours
-    "open_at" : "3T0700", # Return only working POIs in provided time
+    # "open_at" : "3T0700", # Return only working POIs in provided time
 } 
 
 response = requests.request("GET", url, headers=headers, params=params)
 data = response.json()
 
 for idx, place in enumerate(data["results"]):
-    print(idx + 1, place["location"])
+    print(idx + 1, place["name"])
 
 # All POI's features:
 '''
