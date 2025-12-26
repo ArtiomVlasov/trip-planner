3292563fc87b25a0be595bf2eceb597823e577c2
ArtiomVlasov <artiom.lvlasov@gmail.com>
2025-12-26 12:48:00 +0100

    fix bug

--- components/backend/services/collect_places_guest.py
+++ components/backend/services/collect_places_guest.py
@@ -12,7 +12,9 @@ def collect_places_guest(db, parsed_prompt: dict):
     preferred_subtypes = parsed_prompt["user"].get("prefered_type", {}).get("preferred_subtypes") or []
     if not preferred_subtypes:
         print("No preferred subtypes found in prompt")
-
+        
+    hotel_query_text = f"Find hotel in {location_text}"
+    
     hotel_query = search_places(
         db=db,
         user_id=None,
@@ -48,10 +50,10 @@ def collect_places_guest(db, parsed_prompt: dict):
             raw_params=None,
             max_pages=1
         )
-        print("Places found:", len(q.places))
 
-        link = q.places[0] if q.places else None
-        if not link:
+        places_links = db.query(SearchQueryPlace).filter(SearchQueryPlace.query_id == q.id).all()
+        place = places_links[0].place if places_links[0].place else None
+        if not place:
             continue
         
 
