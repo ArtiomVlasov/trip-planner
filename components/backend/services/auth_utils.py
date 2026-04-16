42e1d9891a009183ad4f702ae5247e7e1153d93c
ChxirMac <zyryanov2005@gmail.com>
2026-04-16 16:40:33 +0700

    fix: update access token expiration to use timezone-aware datetime

--- components/backend/services/auth_utils.py
+++ components/backend/services/auth_utils.py
@@ -1,7 +1,7 @@
 import os
 from dotenv import load_dotenv
 import jwt as pyjwt
-from datetime import datetime, timedelta
+from datetime import datetime, timedelta, timezone
 
 load_dotenv(dotenv_path=os.path.join(os.path.dirname(__file__), "../", ".env"))
 
@@ -11,7 +11,7 @@ ACCESS_TOKEN_EXPIRE_MINUTES = 60 * 24
 
 def create_access_token(data: dict):
     to_encode = data.copy()
-    expire = datetime.utcnow() + timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
+    expire = datetime.now(timezone.utc) + timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
     to_encode.update({"exp": expire})
     encoded_jwt = pyjwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
     return encoded_jwt
